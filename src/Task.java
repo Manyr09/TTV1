ede897f631f8a2d8ab44cab33fbb6e6e505bbd10
Manyr09 <fxh.clashofclans@gmail.com>
2024-12-28 11:01:04 +0500

    Add files via upload

---
+++ src/Task.java
@@ -0,0 +1,89 @@
+public class Task {
+    private int id;
+    private String name;
+    private String description;
+    private TaskStatus status;
+
+    public Task(String name, String description, TaskStatus status) {
+
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+
+        this.id = id;
+
+    }
+
+
+
+    public String getName() {
+
+        return name;
+
+    }
+
+
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+
+
+    public String getDescription() {
+
+        return description;
+    }
+
+
+
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+
+
+
+    public void setStatus(TaskStatus status) {
+
+        this.status = status;
+    }
+
+
+
+    @Override
+    public String toString(){
+        return String.format("Задача %s: %s - %s. Статус - %s", getId(), getName(), getDescription(), getStatus());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
