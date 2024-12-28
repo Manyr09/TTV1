9a3b7bc96d70b78b84a35f9418c34fafe916e382
Manyr09 <fxh.clashofclans@gmail.com>
2024-12-28 11:08:18 +0500

    Delete TaskTraserV2/src directory

--- TaskTraserV2/src/HistoryManager.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-import java.util.Collection;
-public interface HistoryManager {
-    void add(Task task);
-    List<Task> getHistory();
-    void remove(int id);
-    void remove(Collection<Integer> ids);
-}
